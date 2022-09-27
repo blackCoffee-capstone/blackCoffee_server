55769cf268318639a46959ea69d31d04a96b06ef
sumin21 <tnals1178@naver.com>
2022-09-27 20:14:23 +0900

    Chore: Setting swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+
+	//TODO:
+	const port = 3000;
+
+	const config = new DocumentBuilder()
+		.setTitle('Test API')
+		.setDescription('The test API description')
+		.setVersion('1.0')
+		.addTag('test')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(port);
+	console.log(`==================listening on port ${port}==================`);
 
 	if (module.hot) {
 		module.hot.accept();
