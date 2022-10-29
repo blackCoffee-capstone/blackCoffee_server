a7068a3fbc1acb225d7a32d24b281f45a65fa02d
alsrud991026 <alsrud991026@naver.com>
2022-10-29 20:20:51 +0900

    Style: Design admin login UI

---
+++ front/js/admin-login.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	$('#login').on('click', function () {
+		var param = {};
+
+		param.email = $('#email').val();
+		param.password = $('#password').val();
+
+		if (email == '') {
+			alert('아이디를 입력해주세요.');
+		} else if (password == '') {
+			alert('비밀번호를 입력해주세요.');
+		}
+
+		// $.ajax({
+		// 	url:
+		// 	data: param,
+		// 	type: 'POST',
+		// 	dataType: 'JSON',
+		// });
+
+		//TODO : 통신처리
+	});
+});
