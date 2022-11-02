60d257d61a69d6a2908eb2bc4f8f7708c67a5ca8
jyeun722 <skqhs7276@gamil.com>
2022-11-02 23:27:05 +0900

    Chore: Pull branch develop into spots-search

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,12 @@ import { DatabaseModule } from './database/database.module';
 import { MailerModule } from './mailer/mailer.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { ViewController } from './views/view.controller';
 import { SpotsModule } from './modules/spots/spots.module';
 
 @Module({
 	imports: [ConfigModule, UsersModule, DatabaseModule, AuthModule, MailerModule, SpotsModule],
-	controllers: [AppController],
+	controllers: [AppController, ViewController],
 	providers: [AppService],
 })
 export class AppModule {}
