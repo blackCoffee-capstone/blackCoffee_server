d4971ad7c8f24452812bf65f9590cf83057da6b4
jyeun722 <skqhs7276@gamil.com>
2022-11-08 16:43:28 +0900

    Chore: Modify varchar nullable false

--- src/entities/theme.entity.ts
+++ src/entities/theme.entity.ts
@@ -6,7 +6,7 @@ import { SnsPost } from './sns-posts.entity';
 @Entity()
 export class Theme extends CommonEntity {
 	@IsString()
-	@Column({ type: 'varchar', length: 20, nullable: true, unique: true })
+	@Column({ type: 'varchar', length: 20, nullable: false, unique: true })
 	name: string;
 
 	@OneToMany(() => SnsPost, (snsPost: SnsPost) => snsPost.theme, {
