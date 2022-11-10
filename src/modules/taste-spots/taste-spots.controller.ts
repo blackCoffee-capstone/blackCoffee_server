a9435e107eb7cfec7443a6d85f99bb1a68b7ddc8
sumin21 <tnals1178@naver.com>
2022-11-10 15:31:26 +0900

    Docs: Add taste-spots to swagger

--- src/modules/taste-spots/taste-spots.controller.ts
+++ src/modules/taste-spots/taste-spots.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { ApiDocs } from './taste-spots.docs';
 import { TasteSpotsService } from './taste-spots.service';
 
 @Controller('taste-spots')
+@ApiTags('tasteSpots - 여행지 취향 정보')
+@UseGuards(JwtAuthGuard)
 export class TasteSpotsController {
 	constructor(private readonly tasteSpotsService: TasteSpotsService) {}
 
 	@Get()
+	@ApiDocs.getTasteSpots('여행지 취향 선택 리스트 반환')
 	async getTasteSpots(@Query('length') length: number) {
 		return await this.tasteSpotsService.getTasteSpots(length);
 	}
