7ba385b163195ce7e83108090bf55f60bb1900b3
sumin21 <tnals1178@naver.com>
2022-11-14 12:40:48 +0900

    Test: add test code

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { TasteSpot } from 'src/entities/taste-spots.entity';
 
+import { Spot } from 'src/entities/spots.entity';
+import { TasteSpot } from 'src/entities/taste-spots.entity';
 import { User } from 'src/entities/users.entity';
+import { MockSpotsRepository } from 'test/mock/spots.mock';
 import { MockTasteSpotsRepository } from 'test/mock/taste-spots.mock';
 import { MockUsersRepository } from 'test/mock/users.mock';
 import { UsersController } from './users.controller';
@@ -23,6 +25,10 @@ describe('UsersController', () => {
 				{
 					provide: getRepositoryToken(TasteSpot),
 					useClass: MockTasteSpotsRepository,
+				},
+				{
+					provide: getRepositoryToken(Spot),
+					useClass: MockSpotsRepository,
 				},
 			],
 		}).compile();
