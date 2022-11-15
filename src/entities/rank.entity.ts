22b14da6e1b2e2807494a785ad11ef69d24704a5
jyeun722 <skqhs7276@gamil.com>
2022-11-15 21:05:12 +0900

    Feat: Connect rank with spot entity manytoone

--- src/entities/rank.entity.ts
+++ src/entities/rank.entity.ts
@@ -1,7 +1,8 @@
 import { IsNumber } from 'class-validator';
-import { Column, Entity, Index } from 'typeorm';
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
 
 import { CommonEntity } from './common.entity';
+import { Spot } from './spots.entity';
 
 @Entity()
 @Index(['year', 'month', 'week', 'spotId', 'rank'], { unique: true })
@@ -21,6 +22,12 @@ export class Rank extends CommonEntity {
 	@IsNumber()
 	@Column({ name: 'spot_id', type: 'smallint', nullable: false })
 	spotId: number;
+
+	@ManyToOne(() => Spot, (spot: Spot) => spot.ranks, {
+		onDelete: 'NO ACTION',
+	})
+	@JoinColumn([{ name: 'spot_id', referencedColumnName: 'id' }])
+	spot: Spot;
 
 	@IsNumber()
 	@Column({ type: 'smallint', nullable: false })
