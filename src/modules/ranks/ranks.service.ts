d1a954f38a873c87f4818494550a7cc8c96a3d26
jyeun722 <skqhs7276@gamil.com>
2022-11-19 06:33:57 +0900

    Feat: Add date exception and before week calculation

--- src/modules/ranks/ranks.service.ts
+++ src/modules/ranks/ranks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -19,23 +19,45 @@ export class RanksService {
 		private readonly ranksRepository: Repository<Rank>,
 	) {}
 
-	async filterWeek(week: number, beforeFilterWeek) {
-		const afterFilterWeek = beforeFilterWeek
-			.where('rankings.week = :week', { week: week })
-			.orderBy('rankings.rank', 'ASC')
-			.getRawMany();
-		console.log(afterFilterWeek);
-		return afterFilterWeek;
+	async checkWeek(rankingRequest: RankingRequestDto) {
+		const nextCheckWeek = await this.ranksRepository.createQueryBuilder('rank').orderBy('date', 'DESC').getOne();
+		if (!nextCheckWeek) throw new NotFoundException('Rank is not found');
+		if (rankingRequest.date > nextCheckWeek.date) throw new NotFoundException('Next is not found');
+
+		const previousCheckWeek = await this.ranksRepository.createQueryBuilder('rank').orderBy('date', 'ASC').getOne();
+		if (rankingRequest.date < previousCheckWeek.date) throw new NotFoundException('Previous is not found');
+	}
+
+	async beforeWeek(rankingRequest: RankingRequestDto) {
+		let beforeDate = rankingRequest.date - 1;
+		if ((rankingRequest.date - 1) / 10) {
+			const beforeCheckWeek = await this.ranksRepository
+				.createQueryBuilder('rank')
+				.select('date')
+				.distinctOn(['date'])
+				.orderBy('date', 'ASC')
+				.getRawMany();
+
+			const weekList = Array.from(beforeCheckWeek).flatMap(({ date }) => [date]);
+			const dateIdx = weekList.lastIndexOf(rankingRequest.date);
+			if (dateIdx === -1) throw new BadRequestException('Date is not exist');
+			beforeDate = weekList[dateIdx - 1];
+		}
+		return beforeDate;
 	}
 
 	async getRanksList(rankingRequest: RankingRequestDto) {
+		await this.checkWeek(rankingRequest);
+		const beforeDate = await this.beforeWeek(rankingRequest);
+
 		try {
 			const rankingListSpots = await this.spotsRepository
 				.createQueryBuilder('spot')
 				.innerJoinAndSelect('spot.location', 'location')
+				.innerJoinAndSelect('spot.rankings', 'rankings')
+				.where('rankings.date = :date', { date: rankingRequest.date })
 				.select('spot.id AS id, spot.name AS name')
 				.addSelect('location.id AS location_id, location.metroName AS metro, location.localName AS local')
-				.where('spot.rank is not null')
 				.addSelect((afterRank) => {
 					return afterRank
 						.select('rankings.rank', 'after_rank')
@@ -47,31 +69,36 @@ export class RanksService {
 				.addSelect((berforRank) => {
 					return berforRank
 						.select('rankings.rank', 'before_rank')
-						.where('rankings.Date = :beforeDate', { beforeDate: rankingRequest.date - 1 })
+						.where('rankings.Date = :beforeDate', { beforeDate: beforeDate })
 						.andWhere('rankings.spotId = spot.id')
 						.from(Rank, 'rankings')
 						.limit(1);
 				}, 'before_rank')
 				.getRawMany();
 
-			return Array.from(rankingListSpots).map(
-				(post) =>
-					new RankingListResponseDto({
-						...post,
-						variance: post.before_rank - post.current_rank,
-						location: new LocationResponseDto({
-							id: post.location_id,
-							metroName: post.metro,
-							localName: post.local,
-						}),
+			return Array.from(rankingListSpots).map(function (post) {
+				let variance = null;
+				if (post.before_rank) variance = post.before_rank - post.after_rank;
+				return new RankingListResponseDto({
+					...post,
+					rank: post.after_rank,
+					variance: variance,
+					location: new LocationResponseDto({
+						id: post.location_id,
+						metroName: post.metro,
+						localName: post.local,
 					}),
-			);
+				});
+			});
 		} catch (error) {
 			throw new InternalServerErrorException(error.message, error);
 		}
 	}
 
 	async getRanksMap(rankingRequest: RankingRequestDto) {
+		await this.checkWeek(rankingRequest);
+		await this.beforeWeek(rankingRequest);
+
 		try {
 			const rankingMapSpots = await this.spotsRepository
 				.createQueryBuilder('spot')
