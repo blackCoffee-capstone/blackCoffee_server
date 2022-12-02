75b02513f15bd6401faa37d3fed0ebaa854c39ac
sumin21 <tnals1178@naver.com>
2022-12-02 23:53:52 +0900

    Fix: Fix test code (recomm module)

--- src/modules/recommendations/recommendations.controller.spec.ts
+++ src/modules/recommendations/recommendations.controller.spec.ts
@@ -1,9 +1,11 @@
 import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { ClickSpot } from 'src/entities/click-spots.entity';
 import { Spot } from 'src/entities/spots.entity';
 import { TasteTheme } from 'src/entities/taste-themes.entity';
 import { WishSpot } from 'src/entities/wish-spots.entity';
+import { MockClickSpotsRepository } from 'test/mock/click-spots.mock';
 import { MockSpotsRepository } from 'test/mock/spots.mock';
 import { MockTasteThemesRepository } from 'test/mock/taste-themes.mock';
 import { MockWishSpotsRepository } from 'test/mock/wish-spots.mock';
@@ -29,6 +31,10 @@ describe('RecommendationsController', () => {
 				{
 					provide: getRepositoryToken(WishSpot),
 					useClass: MockWishSpotsRepository,
+				},
+				{
+					provide: getRepositoryToken(ClickSpot),
+					useClass: MockClickSpotsRepository,
 				},
 				{
 					provide: ConfigService,
