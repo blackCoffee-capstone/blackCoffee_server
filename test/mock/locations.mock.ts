992c4d6f918b5137a14c7d98ae61b6b77e93ed3f
jyeun722 <skqhs7276@gamil.com>
2022-12-10 03:19:19 +0900

    Test: Add test code (filters)

--- test/mock/locations.mock.ts
+++ test/mock/locations.mock.ts
@@ -1,8 +1,38 @@
 export const mockLocation = [
 	{
+		id: 1,
+		metroName: '인천',
+		localName: null,
+	},
+	{
 		id: 2,
 		metroName: '인천',
 		localName: '연수구',
+	},
+	{
+		id: 3,
+		metroName: '인천',
+		localName: '계양구',
+	},
+];
+
+export const mockMetroLocation = [
+	{
+		id: 1,
+		metroName: '인천',
+		localName: null,
+	},
+];
+
+export const mockFilterLocation = [
+	{
+		id: 1,
+		level: 1,
+		metro_name: '인천',
+		locals: [
+			{ id: 2, level: 2, localName: '연수구' },
+			{ id: 3, level: 2, localName: '계양구' },
+		],
 	},
 ];
 
@@ -10,6 +40,9 @@ export class MockLocationsRepository {
 	save = jest.fn();
 	findOne = jest.fn().mockReturnThis();
 	concat = jest.fn().mockReturnThis();
+	flatMap = jest.fn().mockReturnValue({
+		call: jest.fn().mockReturnThis(),
+	});
 	createQueryBuilder = jest.fn().mockReturnValue({
 		leftJoinAndSelect: jest.fn().mockReturnThis(),
 		select: jest.fn().mockReturnThis(),
@@ -21,5 +54,11 @@ export class MockLocationsRepository {
 
 	async find() {
 		return mockLocation;
+	}
+	async findFilter() {
+		return mockFilterLocation;
+	}
+	async findMetro() {
+		return mockMetroLocation;
 	}
 }
