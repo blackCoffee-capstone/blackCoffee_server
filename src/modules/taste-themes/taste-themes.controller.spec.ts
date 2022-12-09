8d2588f05acfdaca376e20de0940837cc02fa5b9
sumin21 <tnals1178@naver.com>
2022-12-10 04:58:21 +0900

    Test: Add test code to taste-theme module

--- src/modules/taste-themes/taste-themes.controller.spec.ts
+++ src/modules/taste-themes/taste-themes.controller.spec.ts
@@ -8,6 +8,7 @@ import { TasteThemesService } from './taste-themes.service';
 
 describe('TasteThemesController', () => {
 	let tasteThemesController: TasteThemesController;
+	let themesRepository: MockThemeRepository;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -22,9 +23,26 @@ describe('TasteThemesController', () => {
 		}).compile();
 
 		tasteThemesController = module.get<TasteThemesController>(TasteThemesController);
+		themesRepository = module.get(getRepositoryToken(Theme));
 	});
 
 	it('should be defined', () => {
 		expect(tasteThemesController).toBeDefined();
+	});
+	describe('getTasteThemes()', () => {
+		it('테마취향 선택 리스트를 반환한다.', async () => {
+			const themes = await themesRepository.find();
+			const themeDtos = [];
+			for (const theme of themes) {
+				themeDtos.push({
+					id: theme.id,
+					name: theme.name,
+					photo_url: theme.photoUrl,
+				});
+			}
+			themesRepository.createQueryBuilder().getRawMany.mockResolvedValue(themeDtos);
+
+			await expect(tasteThemesController.getTasteThemes()).resolves.toEqual(themes);
+		});
 	});
 });
