6b4e305528ecbce8420256c3f1a9e0d271d2543d
Minkyung Kong <alsrud991026@naver.com>
2022-12-10 15:03:07 +0900

    Feat: Add test code report-posts

--- src/modules/report-posts/report-posts.controller.spec.ts
+++ src/modules/report-posts/report-posts.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { getMaxListeners } from 'process';
 import { ReportPost } from 'src/entities/report-posts.entity';
+import { AdFormType } from 'src/types/ad-form.types';
 import { MockReportPostsRepository } from 'test/mock/report-posts.mock';
+import { ReportsResponseDto } from './dto/reports-response.dto';
 import { ReportPostsController } from './report-posts.controller';
 import { ReportPostsService } from './report-posts.service';
 
 describe('ReportPostsController', () => {
-	let controller: ReportPostsController;
+	let reportPostscontroller: ReportPostsController;
+	let reportPostsRepository: MockReportPostsRepository;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -20,10 +24,31 @@ describe('ReportPostsController', () => {
 			],
 		}).compile();
 
-		controller = module.get<ReportPostsController>(ReportPostsController);
+		reportPostscontroller = module.get<ReportPostsController>(ReportPostsController);
+		reportPostsRepository = module.get(getRepositoryToken(ReportPost));
 	});
 
 	it('should be defined', () => {
-		expect(controller).toBeDefined();
+		expect(reportPostscontroller).toBeDefined();
+	});
+
+	describe('getAllReport()', () => {
+		it('신고 목록을 반환한다.', async () => {
+			const reports = await reportPostsRepository.find();
+			const expectReports = reports.map((item) => new ReportsResponseDto(item));
+			reportPostsRepository.createQueryBuilder().getMany.mockResolvedValue(expectReports);
+
+			await expect(reportPostscontroller.getAllReports({})).resolves.toEqual(expectReports);
+		});
+	});
+
+	describe('getAllReport()', () => {
+		it('신고 목록을 반환한다.', async () => {
+			const reports = await reportPostsRepository.find();
+			const expectReports = reports.map((item) => new ReportsResponseDto(item));
+			reportPostsRepository.createQueryBuilder().getMany.mockResolvedValue(expectReports);
+
+			await expect(reportPostscontroller.getAllReports({})).resolves.toEqual(expectReports);
+		});
 	});
 });
