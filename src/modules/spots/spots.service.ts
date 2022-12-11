c20bb1002db9066d6a9c1682162f53728b388dd1
sumin21 <tnals1178@naver.com>
2022-12-12 04:13:33 +0900

    Feat: Add delete spot func when no sns-posts

--- src/modules/spots/spots.service.ts
+++ src/modules/spots/spots.service.ts
@@ -256,9 +256,28 @@ export class SpotsService {
 		const snsPhotos: SnsPhotoJsonDto[] = await this.readCsvSnsPostPhotoUrls(
 			path.resolve('src/database/datas', fileName),
 		);
+
 		for (const snsPhoto of snsPhotos) {
-			if (snsPhoto.photoUrl === '-') await this.snsPostRepository.delete({ snsPostUrl: snsPhoto.snsPostUrl });
-			else {
+			if (snsPhoto.photoUrl === '-') {
+				const deleteSpotIds = [];
+				await this.snsPostRepository.delete({ snsPostUrl: snsPhoto.snsPostUrl });
+				const snsPostsSpot = await this.spotsRepository
+					.createQueryBuilder('spot')
+					.leftJoinAndSelect('spot.snsPosts', 'snsPosts')
+					.where('snsPosts.id is null')
+					.getMany();
+
+				for (const spot of snsPostsSpot) {
+					deleteSpotIds.push(spot.id);
+				}
+
+				await this.spotsRepository
+					.createQueryBuilder('spot')
+					.delete()
+					.from(Spot)
+					.where('id IN (:...ids)', { ids: deleteSpotIds })
+					.execute();
+			} else {
 				await this.snsPostRepository.update(
 					{
 						snsPostUrl: snsPhoto.snsPostUrl,
